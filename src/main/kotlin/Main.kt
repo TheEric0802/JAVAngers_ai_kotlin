e3026311565440c6f56f266b5469329512c4a3f4
Eric Schmidt <eric0802@web.de>
2025-04-08 10:55:35 +0200

    add primeNumber functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    println(primeNumbers(5).joinToString())
+}
+
+fun primeNumbers(n: Int): List<Int> {
+    val primeNumbers = mutableListOf<Int>()
+    var potentialPrime = 2
+    while (primeNumbers.size < n) {
+        if (isPrime(potentialPrime)) {
+            primeNumbers.add(potentialPrime)
+        }
+        potentialPrime++
+    }
+    return primeNumbers
+}
+
+fun isPrime(number: Int): Boolean {
+    for (i in 2 until number) {
+        if (number % i == 0) {
+            return false
+        }
+    }
+    return true
 }
